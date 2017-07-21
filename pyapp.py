1925c86044d17ef3f73e36c4278b6e63bf20ccf5
mmclennan <mclennan.m@gmail.com>
2017-07-21 00:12:21 -0300

    #4 Add common tool version

--- pyapp.py
+++ pyapp.py
@@ -32,12 +32,12 @@ def get_config_value(key_name):
 def query_yes_no(question, default=None):
     """Ask a yes/no question via input() and return their answer.
 
-    "question" is a string that is presented to the user.
-    "default" is the presumed answer if the user just hits <Enter>.
+    :question - is a string that is presented to the user.
+    :default - is the presumed answer if the user just hits <Enter>.
         It must be "yes" (the default), "no" or None (meaning
         an answer is required of the user).
 
-    The "answer" return value is True for "yes" or False for "no".
+    :return - True for "yes" or False for "no".
     """
     valid = {"yes": True, "y": True, "ye": True,
              "no": False, "n": False}
